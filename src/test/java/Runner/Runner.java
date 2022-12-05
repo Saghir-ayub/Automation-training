60c126d4d1123ced56cf2232dfe433715ffa9daa
Saghir Ayub <saghir.ayub@ten10.com>
2022-12-05 12:21:39 +0000

    runner changes

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -3,7 +3,6 @@ package Runner;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-import pageObjects.UserInformation;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/Resources/Features", glue = {"StepDefinition"},tags = "@paymentScenario")
