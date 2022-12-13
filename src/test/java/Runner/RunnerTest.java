cb0c2ecd021436ffb53aa408b5a4c6444c160183
Saghir Ayub <saghir.ayub@ten10.com>
2022-12-13 11:44:42 +0000

    allure and extent reporting added

--- src/test/java/Runner/RunnerTest.java
+++ src/test/java/Runner/RunnerTest.java
@@ -5,6 +5,10 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/Resources/Features", glue = {"StepDefinition"})
+@CucumberOptions(
+        features = "src/test/java/Resources/Features",
+        glue = {"StepDefinition"},
+        plugin = {"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
+)
 public class RunnerTest {
 }
